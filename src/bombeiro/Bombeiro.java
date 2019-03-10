e6875474560b2d127ff08e010290011b31ed9e51
welgt <welgt@git.github.com>
2019-03-10 20:30:16 -0300

    teste commit
    Apenas teste

---
+++ src/bombeiro/Bombeiro.java
@@ -0,0 +1,10 @@
+package bombeiro;
+
+public class Bombeiro {
+
+	public static void main(String[] args) {
+		// TODO Stub de método gerado automaticamente
+        System.out.println(" teste");
+	}
+
+}
